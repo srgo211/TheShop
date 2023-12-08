f70816082127ef2166b450b72f312caa51482ced
Ivan <name.navi96@gmail.com>
2023-12-08 14:14:11 +0300

    СОздание решения под проект

---
+++ scr/Web App/WebShopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
