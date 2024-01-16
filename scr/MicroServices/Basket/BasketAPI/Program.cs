071ee07e5ca5909aecf4e22f992b5f36dbb1a84d
Ivan <name.navi96@gmail.com>
2024-01-16 22:48:20 +0300

    все сервисы через RestAPI

--- scr/MicroServices/Basket/BasketAPI/Program.cs
+++ scr/MicroServices/Basket/BasketAPI/Program.cs
@@ -1,5 +1,3 @@
-using BasketAPI.Services;
-
 namespace BasketAPI
 {
     public class Program
@@ -9,13 +7,27 @@ namespace BasketAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddGrpc();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
-            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
 
             app.Run();
         }
